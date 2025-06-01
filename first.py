f3b305a7e177de5c6956c54e3c42453831c266d2
Hayden Cordeiro <cordeirohayden@gmail.com>
2025-06-01 13:55:05 -0400

    increased epocs to a rediculous amount, reduced Epoch 200000, Error: 0.00048598271837548874

--- first.py
+++ first.py
@@ -111,7 +111,7 @@ target_outputs = [(0, 1), (1, 0), (1, 0), (0, 1)]
 create_network([2,3,2])
 
 # Training the network
-for i in range(2000):
+for i in range(200000):
     errorDuringEpoch = 0
     for idx,input_data in enumerate(input_dataset):
         outputs = forward_pass(list(input_data))
@@ -119,7 +119,7 @@ for i in range(2000):
         # print(f"Input: {input_data}")
         # display_network()
         backpropagation(target_outputs[idx])
-        update_weights(list(input_data), learning_rate=0.5)
+        update_weights(list(input_data), learning_rate=0.1)
     print(f"Epoch {i+1}, Error: {errorDuringEpoch}")
 
 print(forward_pass((0, 0)))
